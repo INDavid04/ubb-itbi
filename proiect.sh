a186b0b0f2bf69b50006c0c84cc756bf13a5698e
INDavid04 <davidirimia04@gmail.com>
2025-01-09 11:50:25 +0200

    error: ./proiect.sh: line 14: [: ==: unary operator expected

--- proiect.sh
+++ proiect.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 verifica_daca_exista_broken_links() {
+    # Salvam primii doi parametrii
     local director="$1"           
     local follow_symlinks="$2"
+    local delete_broken_links="$3"
 
+    # Cautarea recursiva
     for element in "$director"/*; do
         if [ -L "$element" ]; then # link simbolic
             if [ ! -e "$element" ]; then # broken link
                 echo "Broken link: $element -> $(readlink "$element")"
+                if [ $delete_broken_links == "--delete" ]; then
+                    rm $element 
+                fi
             elif [ "$follow_symlinks" = true ] && [ -d "$element" ]; then # director
                 echo "Following symlink directory: $element"
                 verifica_daca_exista_broken_links "$(readlink -f "$element")" "$follow_symlinks"
@@ -19,19 +25,23 @@ verifica_daca_exista_broken_links() {
 }
 
 main() {
-    if [ $# -lt 1 ]; then # nu avem parametrii
+    # Daca nu avem parametrii, afisam un mesaj si iesim
+    if [ $# -lt 1 ]; then
         echo "Usage: $0 <director> [--follow-symlinks]"
         exit 1
     fi
 
+    # Presupunem ca avem un singur parametru, directorul
     local director="$1"
     local follow_symlinks=false
 
-    if [ "$2" = "--follow-symlinks" ]; then # verificam daca al doilea parametru este --follow-symlinks
+    # Verificam daca al doilea parametru este --follow-symlinks
+    if [ "$2" = "--follow-symlinks" ]; then 
         follow_symlinks=true  # urmarim symlinks
     fi
 
-    if [ ! -d "$director" ]; then # daca nu exista directorul
+    # Daca nu exista directorul
+    if [ ! -d "$director" ]; then 
         echo "Error: $director is not a directory"
         exit 1
     fi
